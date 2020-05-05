fc946c286b7149481d9c86ecde447fd236377856
DanielZajkowski <zajkowskidaniel@gmail.com>
2020-05-05 18:09:53 +0200

    Initial commit

---
+++ Source/SpectrumAnalyzer/SpectrumAnalyzerGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SpectrumAnalyzerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPECTRUMANALYZER_API ASpectrumAnalyzerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
